da3619e2fdefbb3ab437dec795cda78da8d02005
Budi Pradnyana <budiprad@gmail.com>
2019-09-10 19:40:56 +0700

    Create kong.rb
    update kong url & sha256

---
+++ kong.rb
@@ -0,0 +1,58 @@
+class Kong < Formula
+  desc "Open source Microservices and API Gateway"
+  homepage "https://docs.konghq.com"
+
+  stable do
+    url "https://bintray.com/kong/kong-community-edition-src/download_file?file_path=dists%2Fkong-community-edition-0.14.0.tar.gz"
+    sha256 "21593e04f7e14624643474cec0eb9c1fe5192622a8c2b45a9eee405c3cf244db"
+    depends_on "kong/kong/openresty@1.13.6.2"
+  end
+
+  devel do
+    url "https://github.com/Kong/kong.git", :tag => "1.3.0rc2"
+    depends_on "kong/kong/openresty@1.15.8.1"
+  end
+
+  head do
+    url "https://github.com/Kong/kong.git", :branch => "next"
+    depends_on "kong/kong/openresty@1.15.8.1"
+  end
+
+  depends_on "libyaml"
+
+  patch :DATA
+
+  def install
+    openresty_prefix = Formula["kong/kong/openresty@1.13.6.2"].prefix
+
+    if build.head? or build.devel? then
+      openresty_prefix = Formula["kong/kong/openresty@1.15.8.1"].prefix
+    end
+
+    luarocks_prefix = openresty_prefix + "luarocks"
+    openssl_prefix = openresty_prefix + "openssl"
+
+    system "#{luarocks_prefix}/bin/luarocks",
+           "--tree=#{prefix}",
+           "make",
+           "CRYPTO_DIR=#{openssl_prefix}",
+           "OPENSSL_DIR=#{openssl_prefix}"
+
+    bin.install "bin/kong"
+  end
+end
+
+# patch Kong default `prefix` to `/usr/local/opt/kong` as `/usr/local/`
+# not writable by non root user on OSX
+__END__
+diff --git a/kong/templates/kong_defaults.lua b/kong/templates/kong_defaults.lua
+index e38b475..7a74a2f 100644
+--- a/kong/templates/kong_defaults.lua
++++ b/kong/templates/kong_defaults.lua
+@@ -1,5 +1,5 @@
+ return [[
+-prefix = /usr/local/kong/
++prefix = /usr/local/opt/kong/
+ log_level = notice
+ proxy_access_log = logs/access.log
+ proxy_error_log = logs/error.log
